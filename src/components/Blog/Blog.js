c5e60b77776dc880aaa6b51d0582ea32ba6571dd
unknown <julianatonu@gmail.com>
2022-09-29 12:21:49 +0600

    added answer of last 2  question

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -16,13 +16,24 @@ const Blog = () => {
          
             <div class="p-2">
               <h5>2. Difference between Props and State  </h5>
-              <p>Ans : 
+              <p>Ans :Props: 1.Props are used to pass data from one component to another. 2. Props are read only. 3.Props can not be modified.
+                State :1. State  changes can be asynchronous.2. State can be modified using setState.becouse we know that setState function is used to update the state.  
               </p>
             </div>
            
             <div class="p-2">
               <h5>3. What are the usecases of useEffect except loading data from api? </h5>
-              <p>Ans : 
+              <p>Ans : 1.Running once on mount: fetch API data
+                <br />
+2.Running on state change: validating input field
+<br />
+3.Running on state change: live filtering
+<br />
+4.Running on state change: trigger animation on new array value
+<br />
+5.Running on props change: update paragraph list on fetched API data update
+<br />
+6.Running on props change: updating fetched API data to get BTC updated price
               </p>
             </div>
            
